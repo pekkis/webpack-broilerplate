136c797e12efbf5145b490dfcf86a9b2a74adfee
Mikko Forsström <pekkis@Mikkos-MacBook-Pro.local>
2020-01-10 14:16:10 +0200

    Pluu 2.

--- packages/webpack-broilerplate-emotion/__tests__/index.test.ts
+++ packages/webpack-broilerplate-emotion/__tests__/index.test.ts
@@ -1,3 +1,19 @@
-test("invalid broilerplate mode throws", () => {
-  expect(true).toBe(false);
+import broilerplate, { pipe, build } from "@dr-kobros/webpack-broilerplate";
+import babelFeature from "@dr-kobros/webpack-broilerplate/dist/features/babel";
+
+import emotionFeature from "../src/index";
+
+test("adds preset", () => {
+  const bp = pipe(
+    broilerplate,
+    babelFeature({
+      browsers: ["not dead"]
+    }),
+    emotionFeature(),
+    build
+  )("development");
+
+  console.log(bp);
+
+  expect(true).toBe(true);
 });
--- packages/webpack-broilerplate-emotion/src/index.ts
+++ packages/webpack-broilerplate-emotion/src/index.ts
@@ -1,7 +1,8 @@
 import { BroilerplateContext } from "@dr-kobros/webpack-broilerplate";
+import { pushBabelPreset } from "@dr-kobros/webpack-broilerplate/dist/features/babel";
 
 const emotionFeature = () => (bp: BroilerplateContext): BroilerplateContext => {
-  return bp;
+  return pushBabelPreset("@emotion/babel-preset-css-prop", bp);
 };
 
 export default emotionFeature;
